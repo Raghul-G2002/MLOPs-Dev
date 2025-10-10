778225028c40a8b8409d8eefb3beca641278e382
Raghul-G2002 <raghulgopal2002@gmail.com>
2025-10-10 18:53:02 +0530

    First commit

---
+++ snowflake/automated_code.py
@@ -0,0 +1,5 @@
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import col, lit
+from snowflake.snowpark.types import StructType, StructField, StringType, IntegerType
+from snowflake.snowpark import DataFrame
+from typing import List, Dict, Any
